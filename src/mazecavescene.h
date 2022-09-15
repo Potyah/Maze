f2f36ce51abe196c9f7c97d6f8180b7c085c23dd
Potyah <apotyanikhin@gmail.com>
2022-09-15 16:38:45 +0700

    init commit

---
+++ src/mazecavescene.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <QDebug>
+#include <QGraphicsScene>
+#include <QGraphicsSceneMouseEvent>
+
+class MazeCaveScene : public QGraphicsScene {
+    Q_OBJECT
+public:
+    explicit MazeCaveScene(QObject *parent = nullptr) : QGraphicsScene{parent} {}
+    QPointF m_MousePosition;
+signals:
+    void mouseLeft(double x, double y);
+    void mouseRight();
+
+private:
+    void mousePressEvent(QGraphicsSceneMouseEvent *event) {
+        if (event->buttons() == Qt::LeftButton) {
+            double x = event->scenePos().x();
+            double y = event->scenePos().y();
+            emit mouseLeft(x, y);
+        }
+
+        event->accept();
+    }
+};
